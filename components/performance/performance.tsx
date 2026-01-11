71a424ca92f1047ac18f8aeaa17ef96a41962016
shujanislam <shujanm.islam231@gmail.com>
2026-01-11 22:54:36 +0530

    added error rate query

--- components/performance/performance.tsx
+++ components/performance/performance.tsx
@@ -1,14 +1,20 @@
-import { fetchApiLatencyTime } from '../../lib/performance-metrics/queries';
+import { fetchApiLatencyTime, fetchErrorRate } from '../../lib/performance-metrics/queries';
 
 import DashboardCard from "@/components/DashboardCard";
 
 export default async function Performance() {
   const api_latency_ms = await fetchApiLatencyTime();
+  const error_rate = await fetchErrorRate();
   return (
     <>
         <DashboardCard
           title="Average API latency [Product View] (in ms)"
           content={api_latency_ms}
+        />
+
+        <DashboardCard
+          title="Error Rate (in %)"
+          content={error_rate}
         />
     </>
   );
--- lib/performance-metrics/queries.ts
+++ lib/performance-metrics/queries.ts
@@ -23,3 +23,21 @@ export const fetchApiLatencyTime = unstable_cache(async() => {
   ["fetchApiLatencyTime"],
   { revalidate: 60 }
 );
+
+export const fetchErrorRate = unstable_cache(async() => {
+  try{
+    const total = await pool.query(`SELECT COUNT(*)::int AS total_logs FROM request_logs`);
+
+    const error_count = await pool.query(`SELECT COUNT(*)::int AS error_rate FROM request_logs WHERE ok = 'f'`);
+ 
+    const error_rate = (error_count.rows[0].error_rate / total.rows[0].total_logs) * 100;
+
+    return error_rate;
+  }
+  catch(err: any){
+    console.log(err.message);
+  }
+}, 
+  ["fetchErrorRate"],
+  { revalidate: 60 }
+);
