618a5c59f8f95579d3cb7c4bfdae07ebf182a3cb
shujanislam <shujanm.islam231@gmail.com>
2026-01-12 23:33:31 +0530

    added margin bottom in performance divs

--- components/DashboardCard.tsx
+++ components/DashboardCard.tsx
@@ -6,7 +6,7 @@ export default function DashboardCard({
   content: React.ReactNode;
 }) {
   return (
-    <div className="border border-black p-4">
+    <div className="border border-black p-4 mb-4">
       <div className="text-sm">{title}</div>
       <div className="mt-2 text-4xl font-bold tabular-nums">{content}</div>
     </div>
